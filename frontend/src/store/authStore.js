310441195ff1b11355742e3ed4d27c98682328a1
khaireddine <ihrissanek@gmail.com>
2024-12-03 01:20:09 +0100

    Front_change_endpoint_Server_Render

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://easyservice-app-1.onrender.com";
 
 export const useAuthStore = create(
   persist(
