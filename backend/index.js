09382268c1ab86c5a20910b67946ddee1ace52c4
khaireddine <ihrissanek@gmail.com>
2024-12-05 10:38:08 +0100

    FixTheIssue

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
 const app = express()
 
-app.use(cors({origin:'https://easy-service-app.vercel.app',credentials:true}));
+app.use(cors({origin:'https://easy-service-app.vercel.app/',credentials:true}));
 app.use(cookieParser());
 app.use(express.json());
 
